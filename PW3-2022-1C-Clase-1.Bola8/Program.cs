8e4cb28fd7e856a59a6cecf39da73b5ef973435c
msegovia-unlam <71356046+msegovia-unlam@users.noreply.github.com>
2022-04-16 21:31:47 -0300

    Se agrego ejercicio de Bola 8

---
+++ PW3-2022-1C-Clase-1.Bola8/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using PW3_2022_1C_Clase_1.Entidades;
+using System;
+
+namespace PW3_2022_1C_Clase_1.Bola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte su pregunta");
+            Console.WriteLine("Pregunta: ");
+
+            string pregunta = Console.ReadLine();
+
+            Console.WriteLine($"{Bola8.PedirRespuestas(pregunta)}");
+            
+
+        }
+    }
+}
---
+++ PW3-2022-1C-Clase-1.Entidades/Bola8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PW3_2022_1C_Clase_1.Entidades
+{
+    public static class Bola8
+    {
+        public static string PedirRespuestas(string pregunta)
+        {
+            Random random = new Random();
+
+
+
+            string[] respuestas = {
+                                "Es cierto.",
+                                "Es decididamente así.",
+                                "Sin lugar a dudas.",
+                                "Sí, definitivamente.",
+                                "Puedes confiar en ello.",
+                                "Como yo lo veo, sí.",
+                                "Lo más probable.",
+                                "Perspectiva buena.",
+                                "Sí.",
+                                "Las señales apuntan a que sí.",
+                                "Respuesta confusa, vuelve a intentarlo.",
+                                "Vuelve a preguntar más tarde.",
+                                "Mejor no decirte ahora.",
+                                "No se puede predecir ahora.",
+                                "Concéntrate y vuelve a preguntar.",
+                                "No cuentes con ello.",
+                                "Mi respuesta es no.",
+                                "Mis fuentes dicen que no.",
+                                "Las perspectivas no son muy buenas.",
+                                "Muy dudoso."
+                                };
+
+            int respRandom = random.Next(respuestas.Length + 1);
+
+            return respuestas[respRandom];
+        }
+    }
+}
